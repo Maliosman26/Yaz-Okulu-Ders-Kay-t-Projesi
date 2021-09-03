cbcc93510f3455d5e16037b6ad315ea22f216901
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-09-03 11:09:44 +0300

    Entity Layer

---
+++ EntityLayer/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EntityLayer
+{
+    public class Class1
+    {
+        private string ad;
+        private string soyad;
+        private int id;
+        private string numara;
+        private string fotograf;
+        private string bakiye;
+    }
+}
