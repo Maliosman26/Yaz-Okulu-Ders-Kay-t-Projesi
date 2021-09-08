a4ec42b3735997c1af53563b10745f9f331f6814
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-09-08 16:22:33 +0300

    Business Logice Layer - Creating 'Dersler' Class

---
+++ BusinessLogicLayer/BLLDers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using EntityLayer;
+
+namespace BusinessLogicLayer
+{
+    public class BLLDers
+    {
+        public static List<EntityDers> BllListele()
+        {
+            return DALDers.DersListesi();
+        }
+    }
+}
