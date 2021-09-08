354746e3cc47ed7b83376e604ba23b683b75032e
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-09-08 11:52:37 +0300

    Student Update Processes

---
+++ Yaz Okulu Ders Kayıt Projesi/OgrenciGuncelle.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yaz_Okulu_Ders_Kayıt_Projesi
+{
+    public partial class OgrenciGuncelle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(Request.QueryString["OGRID"].ToString());
+            Txtid.Text = x.ToString();
+            Txtid.Enabled = false;
+        }
+    }
+}
